bcaafcb4f64967e6f7d061d93d85e9c15ee28bcf
murilob03 <muriloviskboccardo@gmail.com>
2024-02-20 14:28:19 -0300

    implemented bfs procedure

---
+++ grafo.py
@@ -0,0 +1,50 @@
+class Graph:
+    def __init__(self, is_directed, is_weighted):
+        self.is_directed = is_directed
+        self.is_weighted = is_weighted
+        self.graph = {}
+
+    def add_edge(self, source, destination, weight=None):
+        if source not in self.graph:
+            self.graph[source] = []
+        if destination not in self.graph[source]:
+            if self.is_weighted:
+                self.graph[source].append((destination, weight))
+            else:
+                self.graph[source].append(destination)
+
+        if not self.is_directed:
+            if destination not in self.graph:
+                self.graph[destination] = []
+            if source not in self.graph[destination]:
+                if self.is_weighted:
+                    self.graph[destination].append((source, weight))
+                else:
+                    self.graph[destination].append(source)
+
+    # Procedimento de busca em largura
+    def bfs(self, s):
+        # Marca todos os vértices como não visitados
+        visited = {key: False for key in self.graph}
+
+        # Cria uma fila para o BFS
+        queue = []
+        queue.append(s)
+
+        visited[s] = True
+
+        while queue:
+            # Remove um vértice da fila e imprime
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            # Pega todos os vértices adjacentes do vértice desenfileirado
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+
+    def display(self):
+        for source, destinations in self.graph.items():
+            print(f"{source} -> {destinations}")
--- main.py
+++ main.py
@@ -1,52 +1,33 @@
-class Graph:
-    def __init__(self, is_directed, is_weighted):
-        self.is_directed = is_directed
-        self.is_weighted = is_weighted
-        self.graph = {}
-
-    def add_edge(self, source, destination, weight=None):
-        if source not in self.graph:
-            self.graph[source] = []
-        if destination not in self.graph[source]:
-            if self.is_weighted:
-                self.graph[source].append((destination, weight))
-            else:
-                self.graph[source].append(destination)
-
-        if not self.is_directed:
-            if destination not in self.graph:
-                self.graph[destination] = []
-            if source not in self.graph[destination]:
-                if self.is_weighted:
-                    self.graph[destination].append((source, weight))
-                else:
-                    self.graph[destination].append(source)
-
-    def display(self):
-        for source, destinations in self.graph.items():
-            print(f"{source} -> {destinations}")
+from grafo import Graph
 
 
 def input_graph():
-    is_directed = input("Is your graph directed? (y/n): ").lower() == 'y'
-    is_weighted = input("Is your graph weighted? (y/n): ").lower() == 'y'
+    is_directed = input("Is your graph directed? (y/n): ").lower() == "y"
+    is_weighted = input("Is your graph weighted? (y/n): ").lower() == "y"
 
     graph = Graph(is_directed, is_weighted)
 
     while True:
         if is_weighted:
-            edge = input("Input your graph edge (source, destination, weight) or type 'done' to finish: ")
-            if edge.lower() == 'done':
+            edge = input(
+                "Input your graph edge (source, destination, weight) or type 'done' to finish: "
+            )
+            if edge.lower() == "done":
                 break
-            source, destination, weight = map(int, edge.split(','))
+            source, destination, weight = map(str, edge.split(","))
             graph.add_edge(source, destination, weight)
         else:
-            edge = input("Input your graph edge (source, destination) or type 'done' to finish: ")
-            if edge.lower() == 'done':
+            edge = input(
+                "Input your graph edge (source, destination) or type 'done' to finish: "
+            )
+            if edge.lower() == "done":
                 break
-            source, destination = map(int, edge.split(','))
+            source, destination = map(str, edge.split(","))
             graph.add_edge(source, destination)
 
-    graph.display()
+    # graph.display()
+    start = input("Where to start the bfs procedure?")
+    graph.bfs(start)
+
 
 input_graph()
